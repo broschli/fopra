ce2f2203690d9bff937087609c5e5ce877770751
broschli <broschli25@gmail.com>
2026-05-29 18:16:13 +0200

    added example plot

---
+++ josephson_parametric_amplifier/example_plot.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# example for importing data
+# data = np.genfromtxt("./data/measurement_1.csv")
+
+# generate some example data to plot
+xs = np.linspace(-3, 3, 100)
+ys = np.exp(-xs**2)
+
+# use the fopraplot stylesheet
+plt.style.use("../fopraplot.mplstyle")
+
+fig, ax = plt.subplots()
+ax.plot(xs, ys)
+ax.set(xlabel="xlabel", ylabel="ylabel")
+
+# save the figure as pdf in the figs folder
+fig.savefig("./figs/example_plot.pdf")
